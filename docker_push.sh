4cc7ea277aaa335b5f05174de26e08ffc1560cc1
Eduard Romanovskyj <eromanovskyj@odin.com>
2018-05-09 22:36:32 +0300

    Add linux only condition into script

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t eromanovskyj/gongfig:latest .
-docker push eromanovskyj/gongfig:latest
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+
+    # Push new image
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker build -t eromanovskyj/gongfig:latest .
+    docker push eromanovskyj/gongfig:latest
+
+fi
