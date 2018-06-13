b0945ccb50d93c86908f4879e09963249b4d97ed
Eduard Romanovskyj <eromanovskyj@odin.com>
2018-06-13 08:40:00 +0300

    Split export into two functions, add finish message

--- pkg/actions/export.go
+++ pkg/actions/export.go
@@ -7,15 +7,40 @@ import (
 	"log"
 	"encoding/json"
 	"io/ioutil"
+	"fmt"
 )
 
-type resourceConfig interface {}
+type Data interface {}
+
+// All items are contained of data property of json answer
+type resourceConfig struct {
+	Data *Data `json:"data"`
+}
 
 // resourceAnswer contains resource name and its configuration so
 // file writer can compose json with name as a key and complete resource configuration as a value
 type resourceAnswer struct {
 	resourceName string
-	config *resourceConfig
+	config *Data
+}
+
+// Get list of resources by http and pass it to the channel where it will be writed to a disk
+func getResourceList(client *http.Client, writeData chan *resourceAnswer, fullPath string, resource string) {
+	response, err := client.Get(fullPath)
+
+	if err != nil {
+		log.Fatal("Request to Kong admin failed")
+		return
+	}
+
+	defer response.Body.Close()
+
+	var body resourceConfig
+	json.NewDecoder(response.Body).Decode(&body)
+
+	// send only data field for writing in order to write { "service": [items...] } instead of
+	// { "service": {"data": [items...] }}
+	writeData <- &resourceAnswer{resource, body.Data}
 }
 
 func Export(adminUrl string, filePath string) {
@@ -30,26 +55,12 @@ func Export(adminUrl string, filePath string) {
 		uri.Path = resource
 		fullPath := uri.String()
 
-		go func(fullPath string, resource string) {
-			response, err := client.Get(fullPath)
-
-			if err != nil {
-				log.Fatal("Request to Kong admin failed")
-				return
-			}
-
-			defer response.Body.Close()
-
-			var body resourceConfig
-			json.NewDecoder(response.Body).Decode(&body)
-
-			writeData <- &resourceAnswer{resource, &body}
-		}(fullPath, resource)
+		go getResourceList(client, writeData, fullPath, resource)
 
 	}
 
 	resourcesNum := len(Apis)
-	config := map[string]resourceConfig{}
+	config := map[string]Data{}
 
 	// Before writing to a file the program composes json
 	// It waits to obtain from channel exactly the same amount as number of resources
@@ -67,4 +78,5 @@ func Export(adminUrl string, filePath string) {
 
 	jsonAnswer, _ := json.MarshalIndent(config, "", "    ")
 	ioutil.WriteFile(filePath, jsonAnswer, 0644)
+	fmt.Println("Done")
 }
