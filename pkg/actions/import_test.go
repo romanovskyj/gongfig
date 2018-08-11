5f08158ec7219d8098c34926dd26d96099ad4c78
Eduard Romanovskyj <eromanovskyj@odin.com>
2018-08-11 09:56:39 +0300

    Add test for service with route creation

--- pkg/actions/import_test.go
+++ pkg/actions/import_test.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"strings"
 	"net/http/httptest"
+	"encoding/json"
 )
 
 // Run particular test in separate thread and test it exits with non zero value
@@ -61,4 +62,51 @@ func TestBadRequest(t *testing.T) {
 	}
 
 	t.Fatalf("process ran with err %v, want exit status 1", err)
+}
+
+func TestServiceWithRoutesCreated(t *testing.T) {
+	//Create path /services/<service name>/routes
+	routesPathElements := []string{ServicesKey, TestService.Name, RoutesKey}
+	routesPath := strings.Join(routesPathElements, "/")
+
+	serviceCreated := false
+	routeCreated := false
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		w.WriteHeader(http.StatusCreated)
+
+		// Use path without slash ([1:])
+		switch path := request.URL.Path[1:]; path {
+		case ServicesKey:
+			serviceCreated = true
+			var body ServicePrepared
+			json.NewDecoder(request.Body).Decode(&body)
+
+			if body.Name != TestService.Name {
+				t.Error("service name is not correct")
+			}
+
+		case routesPath:
+			routeCreated = true
+			var body RoutePrepared
+			json.NewDecoder(request.Body).Decode(&body)
+
+			if body.Paths[0] != TestService.Routes[0].Paths[0] {
+				t.Error("route path is not correct")
+			}
+		}
+		
+	}))
+
+	defer ts.Close()
+
+	prepareAndCreateService(ts.URL)
+
+	if !serviceCreated {
+		t.Error("Service was not created")
+	}
+
+	if !routeCreated {
+		t.Error("Route was not created")
+	}
 }
